b18e425074b2ddecc0b5edddf28b5d634f9b62f3
macniel <christoph.baehr@live.com>
2020-08-25 17:33:20 +0200

    adds matrix tests

--- app.js
+++ app.js
@@ -106,7 +106,404 @@ app.get('/calculate', (req, res) => {
                 actionType: 'free',
                 typeOfTest: 'none'
             },
-            'drop_object': {
+            'change_ownership': {
+                label: 'Change Ownership of Device',
+                type: 'none',
+                actionType: 'free',
+                typeOfTest: 'extended',
+                testParameter: {
+                    attackerSkill: 'Hardware',
+                    attackerAbility: 'Logic',
+                    attackerLimit: 'Mental',
+                    hasAttackerLimit: 'true',
+                    threshold: '24',
+                    note: 'Interval is 1 hour. A gltich results in a report to the authorities.'
+                }
+            },
+            'brute_force': {
+                label: 'Brute Force',
+                type: 'matrix',
+                actionType: 'complex',
+                typeOfTest: 'opposed',
+                testParameter: {
+                    attackerSkill: 'Cybercombat',
+                    attackerAbility: 'Logic',
+                    attackerLimit: 'Attack',
+                    hasAttackerLimit: 'true',
+                    opposedSkill: 'Willpower',
+                    opposedSkill: 'Firewall',
+                    note: 'places 1 MARK, 2 MARKs (-4 DP), or 3 MARKs (-10 DP) at once; or Hop to another Grid.'
+                }
+            },
+            'change_icon': {
+                label: 'Change Icon',
+                type: 'matrix',
+                actionType: 'simple',
+                typeOfTest: 'none',
+                testParameter: {
+                    note: 'Change the target\'s icon to an icon of your choice.'
+                }
+            },
+            'check_overwatch_score': {
+                label: 'Check Overwatch Score',
+                type: 'matrix',
+                actionType: 'simple',
+                typeOfTest: 'test',
+                testParameter: {
+                    attackerSkill: 'Electronic Warfare',
+                    attackerAbility: 'Logic',
+                    attackerLimit: 'Sleaze',
+                    hasAttackerLimit: 'true',
+                    note: 'vs 6 DP.'
+                }
+            },
+            'control_device': {
+                label: 'Control Device',
+                type: 'matrix',
+                actionType: 'variable',
+                typeOfTest: 'test',
+                testParameter: {
+                    attackerSkill: 'Electronic Warfare',
+                    attackerAbility: 'Logic',
+                    attackerLimit: 'Sleaze',
+                    hasAttackerLimit: 'true',
+                    note: 'WTF? pg. 238'
+                }
+            },
+            'crack_file': {
+                label: 'Crack File',
+                type: 'matrix',
+                actionType: 'complex',
+                typeOfTest: 'staticOpposed',
+                testParameter: {
+                    attackerSkill: 'Hacking',
+                    attackerAbility: 'Logic',
+                    attackerLimit: 'Attack',
+                    hasAttackerLimit: 'true',
+                    opposedValue: 'Protection x 2',
+                    note: 'Requires 1 MARK.'
+                }
+            },
+            'crash_program': {
+                label: 'Crash Program',
+                type: 'matrix',
+                actionType: 'complex',
+                typeOfTest: 'opposed',
+                testParameter: {
+                    attackerSkill: 'Cybercombat',
+                    attackerAbility: 'Logic',
+                    attackerLimit: 'Attack',
+                    hasAttackerLimit: 'true',
+                    opposedSkill: 'Intuition',
+                    opposedAbility: 'Firewall',
+                    note: 'Requires 1 MARK. Disables program with target name, and device needs to be rebooted to start it again.'
+                }
+            },
+            'data_spike': {
+                label: 'Data Spike',
+                type: 'matrix',
+                actionType: 'complex',
+                typeOfTest: 'opposed',
+                testParameter: {
+                    attackerSkill: 'Cybercombat',
+                    attackerAbility: 'Logic',
+                    attackerLimit: 'Attack',
+                    hasAttackerLimit: 'true',
+                    opposedSkill: 'Intuition',
+                    opposedAbility: 'Firewall',
+                    note: 'Adds 2 Damage for each MARK. Soaked with Device Rating + Firewall.'
+                }
+            },
+            'disarm_data_bomb': {
+                label: 'Disarm Data Bomb',
+                type: 'matrix',
+                actionType: 'complex',
+                typeOfTest: 'staticOpposed',
+                testParameter: {
+                    attackerSkill: 'Cybercombat',
+                    attackerAbility: 'Logic',
+                    attackerLimit: 'Attack',
+                    hasAttackerLimit: 'true',
+                    opposedValue: 'Data Bomb Rating x 2',
+                    note: 'On a failure, the data bomb activates.'
+                }
+            },
+            'edit_file': {
+                label: 'Edit File',
+                type: 'matrix',
+                actionType: 'complex',
+                typeOfTest: 'opposed',
+                testParameter: {
+                    attackerSkill: 'Computer',
+                    attackerAbility: 'Logic',
+                    attackerLimit: 'Data Processing',
+                    hasAttackerLimit: 'true',
+                    opposedSkill: 'Intuition',
+                    opposedAbility: 'Firewall',
+                    note: 'Requires 1 MARK. Allows to create, change, copy, delete, or protect any kind of file.'
+                }
+            },
+            'enter_exit_host': {
+                label: 'Enter or Exit Host',
+                type: 'matrix',
+                actionType: 'complex',
+                typeOfTest: 'none',
+                testParameter: {
+                    note: 'Requires 1 MARK.'
+                }
+            },
+            'erase_mark': {
+                label: 'Erase MARK',
+                type: 'matrix',
+                actionType: 'complex',
+                typeOfTest: 'opposed',
+                testParameter: {
+                    attackerSkill: 'Computer',
+                    attackerAbility: 'Logic',
+                    attackerLimit: 'Attack',
+                    hasAttackerLimit: 'true',
+                    opposedSkill: 'Willpower',
+                    opposedAbility: 'Firewall',
+                    note: 'Requires one or three MARKs. Can erase 1, 2 (-4 DP) or 3 (-10 DP) MARKs from the same target icon at once.'
+                }
+            },
+            'erase_matrix_signature': {
+                label: 'Erase Matrix Signature',
+                type: 'matrix',
+                actionType: 'complex',
+                typeOfTest: 'staticOpposed',
+                testParameter: {
+                    attackerSkill: 'Computer',
+                    attackerAbility: 'Resonance',
+                    attackerLimit: 'Attack',
+                    hasAttackerLimit: 'true',
+                    opposedValue: 'Signature Rating x 2',
+                    note: 'You need to have a Resonance rating to attempt this action'
+                }
+            },
+            'format_device': {
+                label: 'Format Device',
+                type: 'matrix',
+                actionType: 'complex',
+                typeOfTest: 'opposed',
+                testParameter: {
+                    attackerSkill: 'Computer',
+                    attackerAbility: 'Logic',
+                    attackerLimit: 'Sleaze',
+                    hasAttackerLimit: 'true',
+                    opposedSkill: 'Willpower',
+                    opposedAbility: 'Firewall',
+                    note: 'Requires 3 MARKs.'
+                }
+            },
+            'grid_hop': {
+                label: 'Grid Hop',
+                type: 'matrix',
+                actionType: 'complex',
+                typeOfTest: 'none',
+                testParameter: {
+                    note: 'You need a subscription to the target grid or you can use Brute Force / Hack on the Fly to hop illegaly to the target grid.'
+                }
+            },
+            'hack_on_the_fly': {
+                label: 'Hack on the fly',
+                type: 'matrix',
+                actionType: 'complex',
+                typeOfTest: 'opposed',
+                testParameter: {
+                    attackerSkill: 'Hacking',
+                    attackerAbility: 'Logic',
+                    attackerLimit: 'Sleaze',
+                    hasAttackerLimit: 'true',
+                    opposedAbility: 'Intuition',
+                    opposedSkill: 'Firewall',
+                    note: 'Every two net hits counts as one hit on a Matrix Perception test. Can place 1 MARK, 2 MARKs (-4 DP) or 3 MARKs (-10 DP) at once. Or hop onto another Grid.'
+                }
+            },
+            'hide': {
+                label: 'Hide',
+                type: 'matrix',
+                actionType: 'complex',
+                typeOfTest: 'opposed',
+                testParameter: {
+                    attackerSkill: 'Electronic Warfare',
+                    attackerAbility: 'Intuition',
+                    attackerLimit: 'Sleaze',
+                    hasAttackerLimit: 'true',
+                    opposedAbility: 'Intuition',
+                    opposedSkill: 'Data Processing',
+                    note: 'Can be used to become unspotted. The Icon that spotted you needs to perform a new Matrix Perception against you. Can\'t be used when MARKed.'
+                }
+            },
+            'invite_mark': {
+                label: 'Invite MARK',
+                type: 'matrix',
+                actionType: 'complex',
+                typeOfTest: 'none',
+                testParameter: {
+                    note: 'Needs to be Owner.'
+                }
+            },
+            'jack_out': {
+                label: 'Hide',
+                type: 'matrix',
+                actionType: 'simple',
+                typeOfTest: 'opposed',
+                testParameter: {
+                    attackerSkill: 'Hardware',
+                    attackerAbility: 'Willpower',
+                    attackerLimit: 'Firewall',
+                    hasAttackerLimit: 'true',
+                    opposedAbility: 'Logic',
+                    opposedSkill: 'Attack',
+                    note: 'The Test is only required when you are link-locked, for each persona that has you link-locked.'
+                }
+            },
+            'jam_signals': {
+                label: 'Jam Signals',
+                type: 'matrix',
+                actionType: 'complex',
+                typeOfTest: 'test',
+                testParameter: {
+                    attackerSkill: 'Electronic Warfare',
+                    attackerAbility: 'Logic',
+                    attackerLimit: 'Attack',
+                    note: 'Hits add to the difficulty for actions against and also by devices inside a zone of 100 meters.'
+                }
+            },
+            'jump_into_rigged_device': {
+                label: 'Jump into rigged Device',
+                type: 'matrix',
+                actionType: 'complex',
+                typeOfTest: 'opposed',
+                testParameter: {
+                    attackerSkill: 'Electronic Warfare',
+                    attackerAbility: 'Logic',
+                    attackerLimit: 'Data Processing',
+                    hasAttackerLimit: 'true',
+                    opposedAbility: 'Willpower',
+                    opposedSkill: 'Firewall',
+                    note: 'Requires 3 MARKs. Can only be done in VR with a control rig and the device needs to have a rigger adaptation. Can\'t jump into a device that is already jumped into.'
+                }
+            },
+            'matrix_perception': {
+                label: 'Matrix Perception',
+                type: 'matrix',
+                actionType: 'complex',
+                typeOfTest: 'opposed',
+                testParameter: {
+                    attackerSkill: 'Computer',
+                    attackerAbility: 'Intuition',
+                    attackerLimit: 'Data Processing',
+                    hasAttackerLimit: 'true',
+                    opposedAbility: 'Logic',
+                    opposedSkill: 'Sleaze',
+                    note: 'Test is simple when the target Icon is farther away than 100 meters and requires 1 hit. Test is opposed against Icons that are running silent.'
+                }
+            },
+            'matrix_search': {
+                label: 'Matrix Search',
+                type: 'matrix_search',
+                actionType: 'special',
+                typeOfTest: 'test',
+                testParameter: {
+                    attackerSkill: 'Computer',
+                    attackerAbility: 'Intuition',
+                    attackerLimit: 'Data Processing',
+                }
+            },
+            'reboot_device': {
+                label: 'Reboot Device',
+                type: 'matrix',
+                actionType: 'complex',
+                typeOfTest: 'opposed',
+                testParameter: {
+                    attackerSkill: 'Computer',
+                    attackerAbility: 'Logic',
+                    attackerLimit: 'Data Processing',
+                    hasAttackerLimit: 'true',
+                    opposedAbility: 'Willpower',
+                    opposedSkill: 'Firewall',
+                    note: 'Requires 3 MARKs. The device shut down and restarts at the end of the following combat turn. Every persona are forced to jack out.'
+                }
+            },
+            'send_message': {
+                label: 'Reboot Device',
+                type: 'matrix',
+                actionType: 'simple',
+                typeOfTest: 'none',
+                testParameter: {
+                    note: 'Requires 0 or 1 MARKs. '
+                }
+            },
+            'set_data_bomb': {
+                label: 'Set Data Bomb',
+                type: 'matrix',
+                actionType: 'complex',
+                typeOfTest: 'staticOpposed',
+                testParameter: {
+                    attackerSkill: 'Software',
+                    attackerAbility: 'Logic',
+                    attackerLimit: 'Sleaze',
+                    hasAttackerLimit: 'true',
+                    opposedValue: 'Device Rating x 2',
+                    note: 'Requires 1 MARK. The Data Bomb deals (Rating up to net hits)d6 Matrix Damage when it goes off. To disable it you can use a passcode.'
+                }
+            },
+            'snoop': {
+                label: 'Snoop',
+                type: 'matrix',
+                actionType: 'complex',
+                typeOfTest: 'opposed',
+                testParameter: {
+                    attackerSkill: 'Electronic Warfare',
+                    attackerAbility: 'Intuition',
+                    attackerLimit: 'Sleaze',
+                    hasAttackerLimit: 'true',
+                    opposedAbility: 'Logic',
+                    opposedSkill: 'Firewall',
+                    note: 'Requires 1 MARK. Allows you to intercept Matrix traffic sent to and from your target as long the target is marked.'
+                }
+            },
+            'spoof_command': {
+                label: 'Spoof Command',
+                type: 'matrix',
+                actionType: 'complex',
+                typeOfTest: 'opposed',
+                testParameter: {
+                    attackerSkill: 'Hacking',
+                    attackerAbility: 'Intuition',
+                    attackerLimit: 'Sleaze',
+                    hasAttackerLimit: 'true',
+                    opposedAbility: 'Logic',
+                    opposedSkill: 'Firewall',
+                    note: 'Requires 1 MARK of the icon you want the command to be sent from. The target must be an device or agent.'
+                }
+            },
+            'switch_interface_mode': {
+                label: 'Switch Interface Mode',
+                type: 'matrix',
+                actionType: 'simple',
+                typeOfTest: 'none',
+                testParameter: {
+                    note: 'You switch your perception from AR to VR. You gain or lose Initiative. You cannot use this when you are link locked.'
+                }
+            },
+            'trace_icon': {
+                label: 'Trace Icon',
+                type: 'matrix',
+                actionType: 'complex',
+                typeOfTest: 'opposed',
+                testParameter: {
+                    attackerSkill: 'Computer',
+                    attackerAbility: 'Intuition',
+                    attackerLimit: 'Data Processing',
+                    hasAttackerLimit: 'true',
+                    opposedAbility: 'Willpower',
+                    opposedSkill: 'Sleaze',
+                    note: 'Requires 2 MARKs. You know the target\'s physical location for as long as you have at least one mark on it.'
+                }
+            },'drop_object': {
                 label: 'Drop Object',
                 type: 'none',
                 actionType: 'free',
@@ -722,6 +1119,7 @@ app.get('/calculate', (req, res) => {
             healing: 'Healing Test',
             melee: 'Melee Attack Test',
             matrix: 'Matrix Test',
+            matrix_search: 'Matrix Search Test',
             ranged: 'Ranged Attack Test',
             vehicle: 'Vehicle Test',
             reload: 'Reload',
@@ -782,8 +1180,38 @@ app.get('/calculate', (req, res) => {
         smartgunPlatform: {
             '+1': 'As Gear',
             '+2': 'As Implant',
-        }
+        },
+        physicalDistance: {
+            0: 'Directly connected or up to 100 meters',
+            1: 'up to 1km',
+            3: 'up to 10km',
+            5: 'up to 100km',
+            8: 'Greater than 100km'
+        },
+        spamZone: {
+            1: 'City downtown',
+            2: 'Sprawl downtown',
+            3: 'Major event or advertising blitz',
+            4: 'Commercial area in a city',
+            5: 'Commercial area in a sprawl',
+            6: 'Massive gathering or during widespread emergency'
+        },
+        staticZone: {
+            1: 'Abandoned building',
+            2: 'Abandoned neighborhood, barrens',
+            3: 'Rural area, abadoned underground area, heavy rain or snow',
+            4: 'Wilderness, severe storm',
+            5: 'Remote place with satellite access only',
+            6: 'Remote, enclosed place (cave, desert ruin)'
+        },
     }
+
+    data.actiongroups = new Set();
+    data.testlength = new Set();
+    Object.values(data.actions).forEach( (predicate) => {
+        data.actiongroups.add(predicate.type);
+        data.testlength.add(predicate.actionType);
+    })
     res.render(data._view, data);
 })
 
